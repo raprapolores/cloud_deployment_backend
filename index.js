50c389764bdc66027a252c62f10d1c896fa32e33
ralph <oloresralph@gmail.com>
2025-03-20 04:17:36 +1300

    Added cors origin

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://cloud-app-b4haceamhabcb2gt.canadacentral-01.azurewebsites.net'
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
