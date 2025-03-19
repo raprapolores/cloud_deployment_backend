39d990310700f24b9116a7d192a5a492b3abb051
ralph <oloresralph@gmail.com>
2025-03-19 19:39:21 +1300

    Added public folder

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const bookRoutes = require('./routes/books');
+const path = require('path');
+
 
 dotenv.config();
 
@@ -12,6 +14,16 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the home page (default route)
+app.get('/', (req, res) => {
+  // Serve index.html when the root URL is accessed
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
